1163e1de91a8ae6afe4473f821003793d3a4dde8
jaanjuta <anna.koltsova@testpro.io>
2024-06-15 22:10:27 -0400

    Homework-17

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,77 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+
+@Test
+    public void addSongToPlaylist(){
+        String expectedSongAddedMessage = "Added 1 song into \"TestPro Playlist.\"";
+
+        //navigate to page
+        navigateToPage();
+        //login
+        enterEmail("demo@testpro.io");
+        enterPassword("te$t$tudent");
+        submit();
+        //search song
+        searchSong("dark");
+        Thread.sleep(1000);
+
+        //click view all button
+        clickViewAllBtn();
+        Thread.sleep(1000);
+
+        //select first song from results
+        selectFirstSong();
+        Thread.sleep(1000);
+
+        //click Add to Button
+        clickAddToBtn();
+        Thread.sleep(1000);
+
+        //choose a playList from menu
+        choosePlaylist();
+        Thread.sleep(1000);
+        
+        //actual vs expected
+        Assert.assertEquals(getAddToPlaylistMessage(),expectedSongAddedMessage);
+
+    }
+
+    public String getAddToPlaylistMessage(){
+       WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+       return notification.getText();
+    }
+
+    public void choosePlaylist() {
+        WebElement playList = driver.findElement
+                (By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'TestPro Playlist')]"));
+        playList.click();
+    }
+
+    public void clickAddToBtn() {
+        WebElement addToBtn = driver.findElement
+                (By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
+        addToBtn.click();
+    }
+
+    public void selectFirstSong() {
+        WebElement firstSong = driver.findElement
+                (By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSong.click();
+    }
+
+    public void clickViewAllBtn() {
+        WebElement viewAllBtn = driver.findElement(By.xpath("button[@data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
+    }
+
+    public void searchSong(String songName) {
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchForm input[type='search']"));
+        searchField.sendKeys(songName);
+    }
+
+
+}
